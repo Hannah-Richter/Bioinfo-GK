49e2ec2f0370b1151d3653b5ac255b2c6ab3f128
merlin-lm-stud <merlin.l.marquard@stud.uni-heidelberg.de>
2026-04-23 14:43:54 +0200

    Prog

--- Merlin's Script.R
+++ Merlin's Script.R
@@ -1,28 +1,41 @@
 library(tidyverse)
 library(dplyr)
 
+rm(list=ls())
+
 load("LUAD_data.RData")
 
-luad_anot <- luad_anot_clean
-luad_anot$paper_patient <- strtrim(luad_anot$barcode, 12)
+anot <- luad_anot_clean
+anot$paper_patient <- strtrim(anot$barcode, 12)
+anot <- anot[, c("barcode", grep("^paper_", colnames(anot), value = TRUE))]
 
-luad_exp <- column_to_rownames(luad_exp_clean, "gene")
+exp <- column_to_rownames(luad_exp_clean, "gene")
 
+# patient_sex <- anot$paper_Sex[!is.na(anot$paper_Sex)]
+# names(patient_sex) <- anot$paper_patient[!is.na(anot$paper_Sex)]
+# patient_sex <- patient_sex[-which(patient_sex=="[unknown]")]
 
-patient_sex <- luad_anot$paper_Sex[!is.na(luad_anot$paper_Sex)]
-names(patient_sex) <- luad_anot$paper_patient[!is.na(luad_anot$paper_Sex)]
-patient_sex <- patient_sex[-which(patient_sex=="[unknown]")]
-
-sum(apply(luad_exp, 2, function(k) sum(is.na(k)))) # no NAs in luad_exp
-
-luad_exp <- apply(luad_exp, 2, function(x) {
+exp <- as.data.frame(apply(exp, 2, function(x) {
   d <- which(x=="[unknown]" | x=="[not available]" | x=="[Not Available]")
   x[d] <- NA
   return(x)
-})
+}))
 
-luad_anot <- apply(luad_anot, 2, function(x) {
+anot <- as.data.frame(apply(anot, 2, function(x) {
   d <- which(x=="[unknown]" | x=="[not available]" | x=="[Not Available]")
   x[d] <- NA
   return(x)
-})
+}))
+
+sum(apply(exp, 2, function(k) sum(is.na(k)))) # no NAs in exp
+sum(apply(anot, 2, function(k) sum(is.na(k)))) # 5623 NAs in anot
+
+sum(apply(anot, 2, function(k) sum(is.na(k))))/{dim(anot)[1]*dim(anot)[2]}
+
+length(grep("paper", colnames(anot)))
+# How many paper_ columns are retained? >> 20
+
+colnames(anot) <- c(colnames(anot)[1], colnames(anot)[-1] %>% substring(7))
+# All data retained apart from staging categorisation
+
+                    
