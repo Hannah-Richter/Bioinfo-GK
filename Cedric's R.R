30ea8e3528681ca11edada49a3141fb5fc3ca9c1
Cejiky <ao260@stud.uni-heidelberg.de>
2026-05-17 13:34:02 +0200

    Update Cedric's R.R

--- Cedric's R.R
+++ Cedric's R.R
@@ -5,24 +5,20 @@ load("LUAD_data.RData")
 
 expression_data = luad_exp_clean
 
-#row1 = expression_data %>% dplyr::slice(1)
-#values = row1 %>% dplyr::select(-gene) %>% unlist(use.names = FALSE)
-
 
 #Histogram to determine logCPM or CPM
 expression_data[1,seq(2,length(expression_data[1,]))]%>% unlist(use.names = FALSE) %>% hist(,breaks= 20)
 
+expression_data[,4] %>% unlist(use.names = FALSE) %>% hist(,breaks= 20)
+
+
 
 #Histogram not highly skewed -> indicates logCPM over CPM
 
-  
+#Shapiro-Wilks test of all genes separately
 norm_test = expression_data[,seq(2,length(expression_data[1,]))] %>% apply(MARGIN =1, function(x) shapiro.test(x))
 
-Shapiro_p_values = c()
-for(i in length(norm_test)) Shapiro_p_values[i] = norm_test[[1]]$p.value
-
-                                        
-                                        
-                                        
-matri = norm_test %>% unlist() %>% matrix(., nrow=4)
-hist(matri[1,])
+#Extract p-values from "Shapiro Test" Object and add to list; plot into histogram
+Shapiro_p_values = list()
+for(i in 1:length(norm_test)) Shapiro_p_values[i]= norm_test[[i]]$p.value
+Shapiro_p_values %>% unlist(use.names = FALSE) %>% hist(,breaks=20)
