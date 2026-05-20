48ff738fccdc4fd86fddca8b9c456041cb600dd0
Hannah-Richter <hannah.richter@stud.uni-heidelberg.de>
2026-05-20 22:26:21 +0200

    Update Clinical Feature Classification.R

--- Clinical Feature Classification.R
+++ Clinical Feature Classification.R
@@ -25,8 +25,47 @@ train_data <- luad_anot_clean_num[train_d, ]
 test_data <- luad_anot_clean_num[-train_d, ]
 
 #calculate a multinomal logistic regression (was too complex with all features so I chose 3: Sex, Tumor.stage, Smoking.status)
-model_logreg <- multinom(paper_expression_subtype ~ paper_Sex + paper_Tumor.stage + paper_Smoking.Status, data = train_data)
+model_logreg1 <- multinom(paper_expression_subtype ~ paper_Sex + paper_Tumor.stage + paper_Smoking.Status, data = train_data)
 summary(model_logreg)
 #choose different features
-model_logreg <- multinom(paper_expression_subtype ~ paper_Sex + paper_Age.at.diagnosis + paper_Smoking.Status, data = luad_anot_clean_num)
+model_logreg2 <- multinom(paper_expression_subtype ~ paper_Sex + paper_Age.at.diagnosis + paper_Smoking.Status, data = luad_anot_clean_num)
 summary(model_logreg)
+#this model with these 3 features makes the most sense to predict expression_subtype (model has the highest accuracy)
+model_logreg <- multinom(paper_expression_subtype ~ paper_Tumor.stage + paper_Age.at.diagnosis + paper_Smoking.Status, data = luad_anot_clean_num)
+summary(model_logreg)
+
+#predicting with the test data
+pred_logreg <- predict(model_logreg, newdata = test_data)
+library(caret)
+
+confusionMatrix(
+  data = pred_logreg,
+  reference = test_data$paper_expression_subtype)
+
+#random forest
+library(randomForest)
+model_randomforest <- randomForest(paper_expression_subtype ~ ., data = train_data, importance = TRUE, ntree = 500)
+#check for the most important features 
+varImpPlot(model_randomforest, main = "Feature Importance")
+#predict with the test data
+pred_randomforest <- predict(model_randomforest, test_data)
+confusionMatrix(pred_randomforest, test_data$paper_expression_subtype)
+
+#k-nearest neighbors
+#first normalize the data (only features not the outcome, expression_subtype is in column 5 so we are excluding it)
+data_preproc <- preProcess(train_data[, -5], method = c("center", "scale"))
+train_norm <- predict(data_preproc, train_data[, -5])
+test_norm <- predict(data_preproc, test_data[, -5])
+
+train_norm$paper_expression_subtype<-train_data$paper_expression_subtype
+test_norm$paper_expression_subtype<-test_data$paper_expression_subtype
+
+#train the model
+set.seed(123)
+model_knn <- train(
+  paper_expression_subtype ~ .,
+  data = train_norm,
+  method = "knn",
+  tuneGrid = expand.grid(k = 1:10),
+  trControl = trainControl(method = "cv", number = 5, classProbs = TRUE)
+)
