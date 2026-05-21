35451ebe4fa57622498d4026d5fa5886f17931d6
Hannah-Richter <hannah.richter@stud.uni-heidelberg.de>
2026-05-21 13:52:55 +0200

    Update Clinical Variable Exploration.R

--- Clinical Variable Exploration.R
+++ Clinical Variable Exploration.R
@@ -19,7 +19,7 @@ luad_anot_clean$paper_Age.at.diagnosis <-
 luad_anot_clean %>%
          filter(!is.na(paper_Age.at.diagnosis)) %>% 
          ggplot(aes(x = paper_Age.at.diagnosis)) +
-         geom_bar()
+         geom_histogram()
 #patients are between 41 and 85 years old
 mean(luad_anot_clean$paper_Age.at.diagnosis, na.rm = TRUE)
 median(luad_anot_clean$paper_Age.at.diagnosis, na.rm = TRUE)
@@ -42,3 +42,12 @@ luad_anot_clean %>%
   ggplot(aes(x = paper_Smoking.Status)) +
   geom_bar()
 
+#Examine Transversion.High.Low 
+table(luad_anot_clean$paper_Transversion.High.Low)
+#Fusions
+table(luad_anot_clean$paper_Fusions)
+#EML4-ALK and CLTC-ROS1 are are the most common fusions, CLTC-ROS1 is present in the prox.-inflam expression subtype and EML4-ALK is present in the TRU subtype
+
+#Explore iCluster and CIMP methylation signature via cross-tabulation
+table(luad_anot_clean$paper_iCluster.Group, luad_anot_clean$paper_CIMP.methylation.signature.)
+#most high CIMp methylation signature samples are in Cluster Group 3 and 4, in Cluster Group 1 are only low CIMP methylation samples
